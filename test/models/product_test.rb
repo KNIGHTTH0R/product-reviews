eef9a8e97bc37402868639f48de51b264f8a08e5
Tamara Reyda <tamara.reyda@gmail.com>
2014-09-13 21:54:05 -0700

    added product model unit tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,52 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:brand].any?
+    assert product.errors[:name].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+    assert product.errors[:category].any?
+  end
+  
+  test "product price must be positive" do
+    product = Product.new(brand: "A New Brand",
+                          name: "Cool Product",
+                          description: "yyy",
+                          image_url: "zzz.jpg",
+                          category: "Lips")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 1
+    assert product.valid?
+  end
+  
+  def new_product(image_url)
+    Product.new(brand: "A New Brand",
+                name: "A Name",
+                description: "yyy",
+                image_url: image_url,
+                price: 1,
+                category: "Skin")
+  end
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end    
+    
 end
