79c19ba5b49240cef39f2cd6093b062dbe779351
Tamara Reyda <tamara.reyda@gmail.com>
2014-09-18 16:41:59 -0700

    added functional tests for catalog controller

--- test/controllers/catalog_controller_test.rb
+++ test/controllers/catalog_controller_test.rb
@@ -4,6 +4,9 @@ class CatalogControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '.entry', 3
+    assert_select 'h3', 'MAC'
+    assert_select 'h4', 'Lipstick'
   end
 
 end
